9ea8bf52752813e783492bd60cf0be77af1f7459
Elliott B. Edwards <elliott.b.edwards@gmail.com>
2014-08-06 21:10:54 -0700

    Initial Parser stuff and starting in on API

---
+++ src/Mutable.Test/ExpectCSharpShould.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using Mona;
+
+namespace Mutable
+{
+    public class ExpectCSharpShould
+    {
+        [Theory]
+        [InlineData("a")]
+        [InlineData("_")]
+        [InlineData("_97")]
+        [InlineData("MonaLisa")]
+        [InlineData("@class")]
+        public void MatchIdentifier(string literal)
+        {
+            var parse = ExpectCSharp.Identifier().Parse(literal);
+            parse.Succeeded().Should().BeTrue();
+            parse.Node.Should().Be(literal);
+        }
+
+        [Theory]
+        [InlineData("9")]
+        [InlineData("^")]
+        [InlineData("class")]
+        public void RejectIdentifier(string literal)
+        {
+            var parse = ExpectCSharp.Identifier().Parse(literal);
+            parse.Succeeded().Should().BeFalse();
+        }
+    }
+}
