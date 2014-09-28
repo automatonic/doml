f6770b3603e31efc555481bbb77d8c5b06592635
Elliott B. Edwards <elliott.b.edwards@gmail.com>
2014-09-27 18:45:25 -0700

    TraceShould

---
+++ src/Literal.Test/TraceShould.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace Literal
+{
+
+    public class TraceShould
+    {
+        [Fact]
+        public void TokenizeAnEmptyString()
+        {
+            Trace
+                .Tokenize(string.Empty)
+                .Should()
+                .BeEmpty();
+        }
+    }
+}
